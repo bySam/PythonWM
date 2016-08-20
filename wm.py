8ccfe76adedcbdb9d0c5efa1151384415eb7ed0a
bysam <samuel.jonsson@outlook.com>
2016-08-20 03:44:08 +0200

    yo lol
    pls

--- wm.py
+++ wm.py
@@ -19,20 +19,20 @@ if layout == 1:
 	w2 = 900
 	h2 = 450
 	
+	if len(args) >= 3:
+		gap = int(args[2])
+		
+	if len(args) >= 4:
+		w1 = int(args[3])
+		
+	if len(args) >= 5:
+		h1 = int(args[4]) - gap/2
+	
 	if len(args) == 7:
-		gap = int(args[6])
-		
-	if len(args) == 3:
-		w1 = int(args[2])
-		
-	if len(args) == 4:
-		h1 = int(args[3]) - gap/2
+		h2 = int(args[6]) - gap/2
 	
-	if len(args) == 6:
-		h2 = int(args[5]) - gap/2
-	
-	if len(args) == 5:
-		w2 = int(args[4])
+	if len(args) >= 6:
+		w2 = int(args[5])
 	
 		
 	x1 = 1927 / 2 - w1/2
@@ -58,20 +58,20 @@ if layout == 2:
 	w2 = 900
 	h2 = 900
 	
+	if len(args) >= 3:
+		gap = int(args[2])
+		
+	if len(args) >= 4:
+		w1 = int(args[3])
+		
+	if len(args) >= 5:
+		h1 = int(args[4]) - gap/2
+	
 	if len(args) == 7:
-		gap = int(args[6])
-		
-	if len(args) == 3:
-		w1 = int(args[2])
-		
-	if len(args) == 4:
-		h1 = int(args[3]) - gap/2
+		h2 = int(args[6]) - gap/2
 	
-	if len(args) == 6:
-		h2 = int(args[5]) - gap/2
-	
-	if len(args) == 5:
-		w2 = int(args[4])
+	if len(args) >= 6:
+		w2 = int(args[5])
 	
 		
 	x1 = (1927 - w1 - w2 - gap) / 2
@@ -82,7 +82,7 @@ if layout == 2:
 	x2 = x1 + w1 + gap
 	
 	
-	y2 = ((1080 - h1)/2)
+	y2 = ((1080 - h2)/2)
 
 	
 	
@@ -96,6 +96,151 @@ if layout == 2:
 	hwnd = win32gui.GetForegroundWindow()
 	win32gui.MoveWindow(hwnd, int(x2), int(y2), int(w2), int(h2), True)
 
+	
+	
+if layout == 3:
+		
+	gap = 20
+	
+	w1 = 900
+	h1 = 600
+	w2 = 920
+	h2 = 900
+	
+	w3 = 900
+	h3 = 300
+	
+	if len(args) >= 3:
+		gap = int(args[2])
+		
+	if len(args) >= 4:
+		w1 = int(args[3])
+		
+	if len(args) >= 5:
+		h1 = int(args[4]) - gap/2
+	
+	if len(args) >= 6:
+		w2 = int(args[5])
+	
+	if len(args) >= 7:
+		h2 = int(args[6]) - gap/2
+
+	if len(args) >= 8:
+		w3 = int(args[7])
+		
+	if len(args) == 9:
+	   h3 = int(args[8]) - gap/2
+	  
+
+	
+		
+	x1 = (1927 - w1 - w2 - gap) / 2
+	
+	y1 = (1080 - h1 - h3 - gap) / 2
+	
+	x3 = (1927 - w3 - w2 - gap) / 2
+	y3 = (y1 + h1 + gap)
+	
+	
+	
+	x2 = x1 + w1 + gap
+	
+	
+	y2 = ((1080 - h2)/2 - gap/2)
+
+	
+	
+	
+	print("Select window 1 and wait 3 sec")
+	sleep(2)
+	hwnd = win32gui.GetForegroundWindow()
+	win32gui.MoveWindow(hwnd, int(x1), int(y1), int(w1), int(h1), True)
+	print("Select window 2 and wait 3 sec")
+	sleep(2)
+	hwnd = win32gui.GetForegroundWindow()
+	win32gui.MoveWindow(hwnd, int(x2), int(y2), int(w2), int(h2), True)
+	
+	print("Select window 3 and wait 3 sec")
+	sleep(2)
+	hwnd = win32gui.GetForegroundWindow()
+	win32gui.MoveWindow(hwnd, int(x3), int(y3), int(w3), int(h3), True)
+	win32gui.MoveWindow(hwnd, int(x3), int(y3), int(w3), int(h3), True)
+	
+if layout == 4:
+		
+	gap = 20
+	
+	w1 = 400
+	h1 = 400
+	w2 = 400
+	h2 = 400
+	
+	w3 = 400
+	h3 = 400
+	
+	w4 = 400
+	h4 = 400
+	
+	if len(args) >= 3:
+		gap = int(args[2])
+		
+	if len(args) >= 4:
+		w1 = int(args[3])
+		
+	if len(args) >= 5:
+		h1 = int(args[4]) - gap/2
+	
+	if len(args) >= 6:
+		w2 = int(args[5])
+	
+	if len(args) >= 7:
+		h2 = int(args[6]) - gap/2
+
+	if len(args) >= 8:
+		w3 = int(args[7])
+		
+	if len(args) >= 9:
+	   h3 = int(args[8]) - gap/2
+	  
+	if len(args) >= 10:
+		w4 = int(args[9])
+		
+	if len(args) == 11:
+		h4 = int(args[10]) - gap/2
+	
+		
+	x1 = (1927 - w1 - w2 - gap) / 2
+	y1 = (1080 - h1 - h3 - gap) / 2
+	x3 = (1927 - w3 - w2 - gap) / 2
+	y3 = (y1 + h1 + gap)
+	
+	y2 = (1080 - h1 - h2 - gap) / 2
+	x2 = x1 + w1 + gap
+	y4 = (y1 + h1 + gap)
+	x4 = x3 + w3 + gap
+	
+	
+	
+	
+	print("Select window 1 and wait 3 sec")
+	sleep(3)
+	hwnd = win32gui.GetForegroundWindow()
+	win32gui.MoveWindow(hwnd, int(x1), int(y1), int(w1), int(h1), True)
+	print("Select window 2 and wait 3 sec")
+	sleep(3)
+	hwnd = win32gui.GetForegroundWindow()
+	win32gui.MoveWindow(hwnd, int(x2), int(y2), int(w2), int(h2), True)
+	
+	print("Select window 3 and wait 3 sec")
+	sleep(3)
+	hwnd = win32gui.GetForegroundWindow()
+	win32gui.MoveWindow(hwnd, int(x3), int(y3), int(w3), int(h3), True)
+	
+	print("Select window 4 and wait 3 sec")
+	sleep(3)
+	hwnd = win32gui.GetForegroundWindow()
+	win32gui.MoveWindow(hwnd, int(x4), int(y4), int(w4), int(h4), True)
+	
 
 
 
