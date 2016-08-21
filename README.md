258883ef584590fb160068ca2f38b9e679504afd
Samuel Jonsson <samuel.jonsson@outlook.com>
2016-08-21 14:50:05 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 # PythonWM
 A semi-tiling manual window manager for windows made in python
-
-Large resolution webm [can be found here](https://gfycat.com/DisguisedWastefulBackswimmer)
-
-![Alt Text](https://thumbs.gfycat.com/DisguisedWastefulBackswimmer-size_restricted.gif)
--- wm.py
+++ wm.py
@@ -241,8 +241,68 @@ if layout == 4:
 	hwnd = win32gui.GetForegroundWindow()
 	win32gui.MoveWindow(hwnd, int(x4), int(y4), int(w4), int(h4), True)
 	
+if layout == 5:
+		
+	gap = 20
+	
+	w1 = 600
+	h1 = 920
+	w2 = 450
+	h2 = 450
+	
+	w3 = 450
+	h3 = 450
+	
+	w4 = 450
+	h4 = 450
+	
+	if len(args) >= 3:
+		gap = int(args[2])
+		
+	if len(args) >= 4:
+		w1 = int(args[3])
+		
+	if len(args) >= 5:
+		h1 = int(args[4]) - gap/2
+	
+	if len(args) >= 6:
+		w2 = int(args[5])
+	
+	if len(args) >= 7:
+		h2 = int(args[6]) - gap/2
 
-
+	if len(args) >= 8:
+		w4 = int(args[7])
+		
+	if len(args) == 9:
+		h4 = int(args[8]) - gap/2
+	
+		
+	x1 = 1927 / 2 - w1
+	y1 = ((1080 - h1)/2)
+	
+	y2 = (1080 - h2 - h4 - gap) / 2
+	x2 = x1 + w1 + gap
+	y4 = (y2 + h2 + gap)
+	x4 = x1 + w1 + gap
+	
+	print(str(y2 + h4 + gap))
+	
+	
+	
+	print("Select window 1 and wait 3 sec")
+	sleep(3)
+	hwnd = win32gui.GetForegroundWindow()
+	win32gui.MoveWindow(hwnd, int(x1), int(y1), int(w1), int(h1), True)
+	print("Select window 2 and wait 3 sec")
+	sleep(3)
+	hwnd = win32gui.GetForegroundWindow()
+	win32gui.MoveWindow(hwnd, int(x2), int(y2), int(w2), int(h2), True)
+	print("Select window 4 and wait 3 sec")
+	sleep(3)
+	hwnd = win32gui.GetForegroundWindow()
+	win32gui.MoveWindow(hwnd, int(x4), int(y4), int(w4), int(h4), True)
+	
 
 
 
